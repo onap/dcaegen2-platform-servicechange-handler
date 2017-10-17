3be31336e2324772c13856da50f4ffd610f4e3cc
Michael Hwang <mhwang@research.att.com>
2017-10-17 12:03:49 -0400

    Add health check script
    Change-Id: I0e51de51b4c205b60e5dc8286be8bc4217e5d5db
    Issue-Id: DCAEGEN2-142
    Signed-off-by: Michael Hwang <mhwang@research.att.com>

---
+++ resources/health.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Health check script for service change handler
+
+# Currently, the simple existence of the running container is assumed to indicate
+# health. A better possible health check is if the distribution client provided a
+# health check call.
+
+exit 0
