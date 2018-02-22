d914d08a70f018468e4b909a780c753ebce243e9
Michael Hwang <mhwang@research.att.com>
2018-02-21 21:50:04 -0500

    Increase unit test coverage
    Also updated pom to use 0.1.1 of oparent
    
    Change-Id: I3af9fd23583bf8de34e3412494d1bc4a99fe4630
    Issue-ID: DCAEGEN2-260
    Signed-off-by: Michael Hwang <mhwang@research.att.com>

--- test/sch/asdc_client_test.clj
+++ test/sch/asdc_client_test.clj
@@ -1,7 +1,7 @@
 ; ============LICENSE_START=======================================================
 ; org.onap.dcae
 ; ================================================================================
-; Copyright (c) 2017 AT&T Intellectual Property. All rights reserved.
+; Copyright (c) 2017-2018 AT&T Intellectual Property. All rights reserved.
 ; ================================================================================
 ; Licensed under the Apache License, Version 2.0 (the "License");
 ; you may not use this file except in compliance with the License.
@@ -20,8 +20,9 @@
 
 (ns sch.asdc-client-test
   (:use (clojure test))
-  (:require [sch.asdc-client :refer [create-asdc-conn]])
+  (:require [sch.asdc-client :as ac])
   )
+
 
 (deftest test-create-asdc-conn
   (let [config-good { :asdcUri "https://asdc-please-work:8443"
@@ -29,7 +30,7 @@
                       :password "foo-password"
                       :consumerId "foo-id"
                       :activateServerTLSAuth true }
-        actual (create-asdc-conn { :asdcDistributionClient config-good })
+        actual (ac/create-asdc-conn { :asdcDistributionClient config-good })
         [uri user password consumer-id insecure?] actual
         ]
     (is (= (str uri) (:asdcUri config-good)))
@@ -37,4 +38,18 @@
     (is (= password (:password config-good)))
     (is (= consumer-id (:consumerId config-good)))
     (is (= insecure? (not (:activateServerTLSAuth config-good))))
+    ))
+
+
+(deftest test-get-consumer-id
+  (let [consumer-id "SOME-CONSUMER-ID"
+        asdc-conn [nil nil nil consumer-id nil]]
+    (is (= consumer-id (ac/get-consumer-id asdc-conn)))
+    ))
+
+
+(deftest test-construct-service-path
+  (let [service-uuid "abc123"]
+    (is (= "/sdc/v1/catalog/services/abc123/metadata"
+           (ac/construct-service-path service-uuid)))
     ))
