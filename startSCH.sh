2b4495aa0a51f3a84b50273fbf391b419cd534e5
Schmalzried, Terry (ts862m) <ts862m@att.com>
2020-07-27 12:31:52 -0400

    Upgrade dependencies
    Delete unused files Dockerfile and startSCH.sh
    
    Issue-ID: DCAEGEN2-2330
    Change-Id: Id2f699b0530545628c77728ffe00740975aad014
    Signed-off-by: Schmalzried, Terry (ts862m) <ts862m@att.com>

--- startSCH.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-grep "^nameserver" /opt/sch/etc/resolv.conf >> /etc/resolv.conf
-
-service sendmail start
-
-java -Dlogback.configurationFile=logback.xml -jar /opt/sch/target/dcae-service-change-handler.jar prod http://consul:8500/v1/kv/service-change-handler?raw=true
-
